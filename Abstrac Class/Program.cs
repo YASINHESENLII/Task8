a602567e255a301bd663bbbf0048b607fdb48102
YASINHESENLII <yasinhesenli44@gmail.com>
2023-04-13 04:45:41 +0400

    Abstract class

--- Abstrac Class/Program.cs
+++ Abstrac Class/Program.cs
@@ -6,60 +6,7 @@ namespace Abstrac_Class
     {
         static void Main(string[] args)
         {
-            //Vehicle[] vehicles = new Vehicle[4];
-
-            //vehicles[0] = new Car()
-            //{
-            //    FactoryName = "Bmw",
-            //    Model = "M5",
-            //    Color = "Black",
-            //    Doorcount = 4,
-            //    IsElectricCar = false,
-            //    DriveTime = 5,
-            //    DrivePath = 150
-            //};
-            //vehicles[1] = new Car()
-            //{
-            //    FactoryName = "Tesla",
-            //    Model = "Model S",
-            //    Color = "White",
-            //    Doorcount = 4,
-            //    IsElectricCar = true,
-            //    DriveTime = 4,
-            //    DrivePath = 200
-            //};
-            //vehicles[2] = new Bicycle()
-            //{
-            //    FactoryName = "Giiant",
-            //    Model = "Talon",
-            //    Color = "Green",
-            //    DriveTime = 2,
-            //    DrivePath = 30,
-            //    Type = "Mountain Bike"
-            //};
-            //vehicles[3] = new Bicycle()
-            //{
-            //    FactoryName = "Scot",
-            //    Model = "Contesa",
-            //    Color = "Blue",
-            //    DriveTime = 3,
-            //    DrivePath = 40,
-            //    Type = "Road Bike"
-            //};
-
-            //foreach (Vehicle vehicle in vehicles)
-            //{
-
-            //    foreach (Vehicle vehicle1 in vehicles)
-            //    {
-            //        Console.WriteLine("\n============================");
-            //        Console.WriteLine(vehicle1);
-            //        //Console.WriteLine("============================");
-            //        //vehicle.DefineNatureHarmness();
-            //        //Console.WriteLine($"Average Speed: {vehicle.Averagespeed()} km/hour");
-            //        //vehicle.GetInfo();
-            //    }
-            //}
+            
             Car car = new Car
             {
                 FactoryName = "Bmw",
@@ -73,14 +20,14 @@ namespace Abstrac_Class
             };
             Bicycle bicycle = new Bicycle
             {
-                FactoryName = "Giant",
-                Model = "Talon",
-                Color = "Blue",
+                FactoryName = "Gant",
+                Model = "Taln",
+                Color = "Red",
                 DriveTime = 2,
                 DrivePath = 100,
                 Type = "Mountain Bike"
             };
-            Vehicle[] vehicle = new Vehicle[] { car };
+            Vehicle[] vehicle = new Vehicle[] { car,bicycle };
 
             foreach (var item in vehicle)
             {
